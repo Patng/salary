f64d0698ee89b54d99246c8b19133a532889830d
Patrick <treeshole@gmail.com>
2013-12-04 23:48:54 +0800

    add user model with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Salary::Application.routes.draw do
 
+  devise_for :users
   root :to => 'pages#home'
   get 'about' => 'pages#about'
   # The priority is based upon order of creation: first created -> highest priority.
